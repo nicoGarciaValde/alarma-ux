7587e60a9164de1eeb62bb4a0e37a21d4be51b83
Jose Carlos Hernandez <jose.hernandez@line-up.com.mx>
2025-03-13 23:15:16 -0600

    - Se agregan dispositivos a a las pantallas"

--- alarma/src/app/features/devices/devices.module.ts
+++ alarma/src/app/features/devices/devices.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DevicesComponent } from './devices.component';
 import { DevicesRoutingModule } from './devices-routing.module';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   imports: [
     CommonModule,
     DevicesRoutingModule,
+    MatListModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSlideToggleModule
   ],
   declarations: [DevicesComponent]
 })
